e942fd34795ea75347fa6f7b97a3b490e64e9be7
KnightRider <omar.brown@comcast.net>
2019-05-03 15:32:05 -0400

    part 2

--- tamagotchi.js
+++ tamagotchi.js
@@ -2,22 +2,29 @@ console.log('tamagotchi file is loaded');
 
 //your class declaration here
 class Tamagotchis{
-    constructor(){
+    constructor(name,creatureType)
+    {
         this.foodInTummy = 10;
         this.restedness = 10;
         this.health = 10;
+        this.name = name;
+        this.creatureType = creatureType;
     }
     cry(){
         this.foodInTummy = this.foodInTummy - 1;
-        console.log (WAHH!!!!);
-        console.log (foodInTummy);l
+        console.log ("WAHH!!!!");
+        console.log (this.name + thisfoodInTummy);
+        
 
     }
 }
 
 //create new Tamagotchis
-const newTamagotchis1 = Tamagotchis1();
-const newTamagotchis2 = Tamagotchis2();
+const newTamagotchis1 = Tamagotchis(){
+    cry()
+} 
+const newTamagotchis2 = Tamagotchis();
+
 
 //test out your Tamagotchies below via console.logs
 console.log(newTamagotchis1);
