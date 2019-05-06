7785f75abd48ec15066bc6096a1640f02c8a550a
KnightRider <omar.brown@comcast.net>
2019-05-05 20:39:37 -0400

    part 3: added new methods

--- tamagotchi.js
+++ tamagotchi.js
@@ -12,20 +12,26 @@ class Tamagotchis{
     }
     cry(){
         this.foodInTummy = this.foodInTummy - 1;
-        console.log ("WAHH!!!!");
-        console.log (this.name + thisfoodInTummy);
-        
-
+        console.log ("WAHH!!!!" + this.name + this.foodInTummy);
     }
+    puke(){
+        this.foodInTummy = this.foodInTummy - 1;
+        console.log(this.foodInTummy + "WAHH!!")
+    }
+    yawn(){
+        this.restedness = this.restedness - 1;
+        console.log (this.name + this.restedness);
+    }
+
 }
 
 //create new Tamagotchis
-const newTamagotchis1 = Tamagotchis(){
-    cry()
-} 
-const newTamagotchis2 = Tamagotchis();
-
-
+const newTamagotchis1 = new Tamagotchis('EastAtlantaTodd', 'Velociraptor');
+const newTamagotchis2 = new Tamagotchis('DecaturDutchessDrea', 'Dragon');
+const newTamagotchis3 = new Tamagotchis('CaliTriggaTramaine', 'Flamingo');
+const newTamagotchis4 = new Tamagotchis('BankheadBranden', 'Parrot');
 //test out your Tamagotchies below via console.logs
-console.log(newTamagotchis1);
-console.log(newTamagotchis2);
+console.log(newTamagotchis1.cry());
+console.log(newTamagotchis2.cry());
+console.log(newTamagotchis3.puke());
+console.log(newTamagotchis4.yawn());
